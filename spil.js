78defb189481ca3b2f496b277e0af8d9fe776327
Yoondaal01 <milnik01@iba.dk>
2024-08-27 14:13:01 +0200

    changes

--- spil.js
+++ spil.js
@@ -23,9 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let points = 0;
     let currentScenario = 0;
 
-    const characterCard = "karakter";  // Placeholder: Assign based on user selection or random draw
-    const startupCard = "Virksomhed";  // Placeholder: Assign based on user selection or random draw
-
+    
     const scenarios = [
         {
             title: "Produktudvikling",
@@ -33,18 +31,18 @@ document.addEventListener('DOMContentLoaded', function () {
             choices: [
                 { 
                     image: "../images/s1-p1.png", 
-                    points: 15, 
-                    feedback: "Dette er en stærk tilgang, da interne eksperter har dyb forståelse for produktet og kan sikre, at forbedringerne er tilpasset virksomhedens overordnede vision og mål. +15 point" 
+                    points: 300, 
+                    feedback: "Dette er en stærk tilgang, da interne eksperter har dyb forståelse for produktet og kan sikre, at forbedringerne er tilpasset virksomhedens overordnede vision og mål. + 300 Guldmønter " 
                 },
                 { 
                     image: "../images/s1-p2.png", 
-                    points: -5, 
-                    feedback: "Outsourcing kan være risikabelt i den tidlige fase, da det kan føre til manglende kontrol over kvaliteten og retningen af udviklingen. -5 point" 
+                    points: 50, 
+                    feedback: "Outsourcing kan være risikabelt i den tidlige fase, da det kan føre til manglende kontrol over kvaliteten og retningen af udviklingen. +50 Guldmønter" 
                 },
                 { 
                     image: "../images/s1-p3.png", 
-                    points: 12, 
-                    feedback: "Dette er en klog beslutning, da det sikrer, at produktet opfylder kundernes behov, hvilket kan føre til bedre markedsaccept. +12 point" 
+                    points: 150, 
+                    feedback: "Dette er en klog beslutning, da det sikrer, at produktet opfylder kundernes behov, hvilket kan føre til bedre markedsaccept. +150 Guldmønter" 
                 },
                 
             ]
@@ -55,18 +53,18 @@ document.addEventListener('DOMContentLoaded', function () {
             choices: [
                 { 
                     image: "../images/s2-p1.png", 
-                    points: 15, 
-                    feedback: "At udnytte interne ressourcer er omkostningseffektivt og sikrer, at marketingstrategien er tæt på virksomhedens kernebudskaber. +15 point" 
+                    points: 300, 
+                    feedback: "At udnytte interne ressourcer er omkostningseffektivt og sikrer, at marketingstrategien er tæt på virksomhedens kernebudskaber. +300 Guldmønter" 
                 },
                 { 
                     image: "../images/s2-p2.png", 
-                    points: -10, 
-                    feedback: "Et eksternt bureau kan være dyrt og muligvis ikke have samme forståelse for produktet og markedet som interne ressourcer. -10 point" 
+                    points: 50, 
+                    feedback: "Et eksternt bureau kan være dyrt og muligvis ikke have samme forståelse for produktet og markedet som interne ressourcer. +50 Guldmønter" 
                 },
                 { 
                     image: "../images/s2-p3.png", 
-                    points: 12, 
-                    feedback: "Partnerskaber kan udvide rækkevidden af markedsføringen og introducere produktet til nye kundesegmenter, hvilket er en omkostningseffektiv strategi. +12 point" 
+                    points: 150, 
+                    feedback: "Partnerskaber kan udvide rækkevidden af markedsføringen og introducere produktet til nye kundesegmenter, hvilket er en omkostningseffektiv strategi. +150 Guldmønter" 
                 },
                 
             ]
@@ -77,18 +75,18 @@ document.addEventListener('DOMContentLoaded', function () {
             choices: [
                 { 
                     image: "../images/s3-p1.png", 
-                    points: 15, 
-                    feedback: "Netværk kan åbne døre til strategiske samarbejder, der kan accelerere væksten uden store kapitaludlæg. +15 point" 
+                    points: 300, 
+                    feedback: "Netværk kan åbne døre til strategiske samarbejder, der kan accelerere væksten uden store kapitaludlæg. +300 Guldmønter" 
                 },
                 { 
                     image: "../images/s3-p2.png", 
-                    points: 10, 
-                    feedback: "Organisk vækst er sund og bæredygtig, men det kan være langsommere sammenlignet med at udnytte strategiske samarbejder. +10 point" 
+                    points: 150, 
+                    feedback: "Organisk vækst er sund og bæredygtig, men det kan være langsommere sammenlignet med at udnytte strategiske samarbejder. +150 Guldmønter" 
                 },
                 { 
                     image: "../images/s3-p3.png", 
-                    points: 8, 
-                    feedback: "At udvide teamet kan øge kapaciteten og bringe nye kompetencer ind, men det er også en kapitalintensiv tilgang. +8 point" 
+                    points: 50, 
+                    feedback: "At udvide teamet kan øge kapaciteten og bringe nye kompetencer ind, men det er også en kapitalintensiv tilgang. +50 Guldmønter" 
                 },
             ]
         },
@@ -98,18 +96,18 @@ document.addEventListener('DOMContentLoaded', function () {
             choices: [
                 { 
                     image: "../images/s4-p1.png", 
-                    points: 12, 
-                    feedback: "Mere data giver bedre indsigt i, hvordan produktet kan forbedres, hvilket kan føre til større kundetilfredshed. +12 point" 
+                    points: 150, 
+                    feedback: "Mere data giver bedre indsigt i, hvordan produktet kan forbedres, hvilket kan føre til større kundetilfredshed. +150 Guldmønter" 
                 },
                 { 
                     image: "../images/s4-p2.png", 
-                    points: 15, 
-                    feedback: "Hurtige tilpasninger baseret på feedback kan forbedre produktets markedsaccept og differentiere det fra konkurrenterne. +15 point" 
+                    points: 300, 
+                    feedback: "Hurtige tilpasninger baseret på feedback kan forbedre produktets markedsaccept og differentiere det fra konkurrenterne. +300 Guldmønter" 
                 },
                 { 
                     image: "../images/s4-p3.png", 
-                    points: -10, 
-                    feedback: "At ignorere feedback kan føre til, at man overser kritiske forbedringsmuligheder, hvilket kan skade produktets succes. -10 point" 
+                    points: 50, 
+                    feedback: "At ignorere feedback kan føre til, at man overser kritiske forbedringsmuligheder, hvilket kan skade produktets succes. +50 Guldmønter" 
                 },
                 
             ]
@@ -120,18 +118,18 @@ document.addEventListener('DOMContentLoaded', function () {
             choices: [
                 { 
                     image: "../images/s5-p1.png", 
-                    points: 5, 
-                    feedback: "En stærkere prototype kan gøre virksomheden mere attraktiv for partnere, men det kan forsinke muligheden for partnerskaber. +5 point" 
+                    points: 50, 
+                    feedback: "En stærkere prototype kan gøre virksomheden mere attraktiv for partnere, men det kan forsinke muligheden for partnerskaber. +50 Guldmønter" 
                 },
                 { 
                     image: "../images/s5-p2.png", 
-                    points: 12, 
-                    feedback: "Mentoren kan åbne døre og sikre bedre forhandlingsvilkår, hvilket øger sandsynligheden for en succesfuld aftale. +12 point" 
+                    points: 300, 
+                    feedback: "Mentoren kan åbne døre og sikre bedre forhandlingsvilkår, hvilket øger sandsynligheden for en succesfuld aftale. +300 Guldmønter" 
                 },
                 { 
                     image: "../images/s5-p3.png", 
-                    points: 10, 
-                    feedback: "Direkte kontakt kan skabe stærke partnerskaber, men det kræver en overbevisende præsentation for at lykkes. +10 point" 
+                    points: 150, 
+                    feedback: "Direkte kontakt kan skabe stærke partnerskaber, men det kræver en overbevisende præsentation for at lykkes. +150 Guldmønter" 
                 },
               
             ]
@@ -142,18 +140,18 @@ document.addEventListener('DOMContentLoaded', function () {
             choices: [
                 { 
                     image: "../images/s6-p1.png", 
-                    points: 9, 
-                    feedback: "At kende konkurrenternes strategi kan hjælpe med at tilpasse og styrke din egen position på markedet. +12 point" 
+                    points: 150, 
+                    feedback: "At kende konkurrenternes strategi kan hjælpe med at tilpasse og styrke din egen position på markedet. +150 Guldmønter" 
                 },
                 { 
                     image: "../images/s6-p2.png", 
-                    points: 15, 
-                    feedback: "Differentiering er afgørende for at skabe en unik markedsposition og undgå priskrig. +15 point" 
+                    points: 300, 
+                    feedback: "Differentiering er afgørende for at skabe en unik markedsposition og undgå priskrig. +300 Guldmønter" 
                 },
                 { 
                     image: "../images/s6-p3.png", 
-                    points: -5, 
-                    feedback: "Priskrig kan skade virksomhedens marginer og værdi på lang sigt; det er en risikabel strategi. -5 point" 
+                    points: 50, 
+                    feedback: "Priskrig kan skade virksomhedens marginer og værdi på lang sigt; det er en risikabel strategi. +50 Guldmønter" 
                 },
                 
             ]
@@ -164,18 +162,18 @@ document.addEventListener('DOMContentLoaded', function () {
             choices: [
                 { 
                     image: "../images/s7-p1.png", 
-                    points: 10, 
-                    feedback: "Kapitalinvestering i produktion kan accelerere væksten, men det er afgørende at sikre, at efterspørgslen er tilstrækkelig. +10 point" 
+                    points: 50, 
+                    feedback: "Kapitalinvestering i produktion kan accelerere væksten, men det er afgørende at sikre, at efterspørgslen er tilstrækkelig. +50 Guldmønter" 
                 },
                 { 
                     image: "../images/s7-p2.png", 
-                    points: 15, 
-                    feedback: "Partnerskaber kan muliggøre hurtigere og mere omkostningseffektiv skalering, hvilket kan være mere bæredygtigt end at udvide internt. +15 point" 
+                    points: 300, 
+                    feedback: "Partnerskaber kan muliggøre hurtigere og mere omkostningseffektiv skalering, hvilket kan være mere bæredygtigt end at udvide internt. +300 Guldmønter" 
                 },
                 { 
                     image: "s7-p3.png", 
-                    points: 12, 
-                    feedback: "At sikre en større kundebase før skalering minimerer risikoen og sikrer en mere stabil indtægtsstrøm. +12 point" 
+                    points: 150, 
+                    feedback: "At sikre en større kundebase før skalering minimerer risikoen og sikrer en mere stabil indtægtsstrøm. +150 Guldmønter" 
                 },
               
             ]
@@ -186,18 +184,18 @@ document.addEventListener('DOMContentLoaded', function () {
             choices: [
                 { 
                     image: "../images/s8-p1.png", 
-                    points: 15, 
-                    feedback: "At bruge interne ressourcer sikrer, at brandstrategien er tæt på virksomhedens vision og mål, hvilket kan styrke markedspositionen. +15 point" 
+                    points: 300, 
+                    feedback: "At bruge interne ressourcer sikrer, at brandstrategien er tæt på virksomhedens vision og mål, hvilket kan styrke markedspositionen. +300 Guldmønter" 
                 },
                 { 
                     image: "../images/s8-p2.png", 
-                    points: -10, 
-                    feedback: "Eksterne bureauer kan være dyre og muligvis ikke forstå virksomhedens kerneidentitet så godt som interne ressourcer. -10 point" 
+                    points: 50, 
+                    feedback: "Eksterne bureauer kan være dyre og muligvis ikke forstå virksomhedens kerneidentitet så godt som interne ressourcer. +50 Guldmønter" 
                 },
                 { 
                     image: "../images/s8-p3.png", 
-                    points: 12, 
-                    feedback: "Feedback fra netværket kan give uvurderlige perspektiver og hjælpe med at forme en brandidentitet, der resonerer på markedet. +12 point" 
+                    points: 150, 
+                    feedback: "Feedback fra netværket kan give uvurderlige perspektiver og hjælpe med at forme en brandidentitet, der resonerer på markedet. +150 Guldmønter" 
                 },
                
             ]
@@ -208,18 +206,18 @@ document.addEventListener('DOMContentLoaded', function () {
             choices: [
                 { 
                     image: "../images/s9-p1.png", 
-                    points: 15, 
-                    feedback: "At bruge interne eksperter sikrer, at teknologiintegrationen er godt tilpasset virksomhedens behov og mål. +15 point" 
+                    points: 300, 
+                    feedback: "At bruge interne eksperter sikrer, at teknologiintegrationen er godt tilpasset virksomhedens behov og mål. +300 Guldmønter" 
                 },
                 { 
                     image: "../images/s9-p2.png", 
-                    points: -5, 
-                    feedback: "Eksterne udviklere kan være dyrere og mindre engagerede i virksomhedens langsigtede succes, hvilket kan føre til suboptimale løsninger. -5 point" 
+                    points: 50, 
+                    feedback: "Eksterne udviklere kan være dyrere og mindre engagerede i virksomhedens langsigtede succes, hvilket kan føre til suboptimale løsninger. +50 Guldmønter" 
                 },
                 { 
                     image: "../images/s9-p3.png", 
-                    points: 10, 
-                    feedback: "BInvestering i teknologi kan give en konkurrencefordel, men det er vigtigt at sikre, at teknologien er den rette for virksomheden. +10 point" 
+                    points: 150, 
+                    feedback: "BInvestering i teknologi kan give en konkurrencefordel, men det er vigtigt at sikre, at teknologien er den rette for virksomheden. +150 Guldmønter" 
                 },
                
             ]
@@ -230,18 +228,18 @@ document.addEventListener('DOMContentLoaded', function () {
             choices: [
                 { 
                     image: "../images/s10-p1.png", 
-                    points: 15, 
-                    feedback: "Netværk kan hjælpe med at identificere de mest troværdige og relevante certificeringsorganer, hvilket kan forbedre produktets markedsaccept. +15 point" 
+                    points: 300, 
+                    feedback: "Netværk kan hjælpe med at identificere de mest troværdige og relevante certificeringsorganer, hvilket kan forbedre produktets markedsaccept. +300 Guldmønter" 
                 },
                 { 
                     image: "../images/s10-p2.png", 
                     points: 10, 
-                    feedback: "Certificering kan differentiere produktet og øge dets tiltrækningskraft, men det er vigtigt at sikre, at investeringen giver tilstrækkelig værdi. +10 point" 
+                    feedback: "Certificering kan differentiere produktet og øge dets tiltrækningskraft, men det er vigtigt at sikre, at investeringen giver tilstrækkelig værdi. +50 Guldmønter" 
                 },
                 { 
                     image: "../images/s10-p3.png", 
-                    points: 12, 
-                    feedback: "Markedsføring af bæredygtighed kan tiltrække en bredere kundebase og styrke brandet, hvilket kan føre til øget salg. +12 point" 
+                    points: 150, 
+                    feedback: "Markedsføring af bæredygtighed kan tiltrække en bredere kundebase og styrke brandet, hvilket kan føre til øget salg. +150 Guldmønter" 
                 },
                 
             ]
@@ -259,13 +257,6 @@ document.addEventListener('DOMContentLoaded', function () {
         updateRoundCounter(); // Update round counter for the first round
         backgroundMusic.play(); // Start the background music
 
-        // Set background images for the character and startup cards
-        characterCardDisplay.style.backgroundImage = "url('images/karakter.png')";
-        startupCardDisplay.style.backgroundImage = "url('images/virksomhed.png')";
-        
-        characterCardDisplay.textContent = "";  // Clear text if you want only the image
-        startupCardDisplay.textContent = "";    // Clear text if you want only the image
-        
         loadScenario(); // Load the first scenario
     }
 
@@ -351,14 +342,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
      function updateRoundCounter() {
         roundCounterDisplay.textContent = currentScenario + 1; // Display the current round number
-    }
+    };
 
     function endGame() {
-        gameScreen.innerHTML = `<h2>Spillet er slut</h2><p>Du fik${points} point.</p>`;
-        if (points >= 100) {
-            gameScreen.innerHTML += "<p>Tillykke! Du har med succes opbygget GreenTech Solutions!!</p>";
+        gameScreen.innerHTML = `
+            <div id="end-game-screen">
+                <h2>Spillet er slut</h2>
+                <p>Du fik ${points} point.</p>`;
+            
+        if (points >= 3000) {
+            gameScreen.innerHTML `<div id="end-game-screen">
+            <img src="images/stars-3.png">
+            <p>Tillykke! Du har med stor succes opbygget GreenTech Solutions og gjort det til en blomstrende virksomhed!</p>
+            <button id="try-again-button">Prøv igen</button>`;
+        } else if (points >= 1500) {
+            gameScreen.innerHTML += `<div id="end-game-screen">
+            <img src="images/stars-2.png">
+            <p>Godt gået! Du har gjort GreenTech Solutions til en stabil virksomhed, men der er stadig plads til vækst</p>
+            <button id="try-again-button">Prøv igen</button>`;
         } else {
-            gameScreen.innerHTML += "<p>Desværre, du opnåede ikke nok point til at få succes. Prøv igen!</p>";
+            gameScreen.innerHTML +=`<div id="end-game-screen">
+            <img src="images/stars-1.png">
+            <p>Desværre, du opnåede ikke nok point til at få succes. Prøv igen!</p>
+            <button id="try-again-button">Prøv igen</button>`;
         }
+
+        const tryAgainButton = document.getElementById('try-again-button');
+        tryAgainButton.addEventListener('click', function() {
+            location.reload(); // Reload the page to start the game again
+        });
+        
+
     }
 });
